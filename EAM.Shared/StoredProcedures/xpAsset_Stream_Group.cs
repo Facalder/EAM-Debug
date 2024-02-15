b4d9cd16692423cbb4b0933c6d66af4e6445d8b5
Facalder <faainamacaldera16@gmail.com>
2024-02-15 18:57:22 +0700

    feat(Merge): Fixing and tidy up the code (reformat) to merge dev-be 15/02/2024 18:57 GMT + 7

---
+++ EAM.Shared/StoredProcedures/xpAsset_Stream_Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAM.Shared.StoredProcedures
+{
+  internal class xpAsset_Stream_Group
+  {
+  }
+}
